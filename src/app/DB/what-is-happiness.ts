a416617025c73e481ff41b3eb4cb8efc1c772516
Mikhail Rychagov <mixrich1@gmail.com>
2023-08-04 23:51:26 +0700

    What is happiness update

--- src/app/DB/what-is-happiness.ts
+++ src/app/DB/what-is-happiness.ts
@@ -15,64 +15,64 @@ export const getWhatIsHappinessProject: () => IProject = () => ({
     {
       src: getSrc('poster.jpg'),
     },
-    {
-      title: TranslateService.localize('project.What-Is-Happiness.prepare-description'),
-      src: getSrc('characters/1.jpg'),
-    },
-    {
-      src: getSrc('characters/2.jpg'),
-    },
-    {
-      src: getSrc('characters/3.jpg'),
-    },
-    {
-      title: TranslateService.localize('project.What-Is-Happiness.storyboard'),
-      src: getSrc('storyboard.jpg'),
-    },
-    {
-      title: TranslateService.localize('project.What-Is-Happiness.templates'),
-      src: getSrc('templates/1,2.jpg'),
-    },
-    {
-      src: getSrc('templates/3.jpg'),
-    },
-    {
-      src: getSrc('templates/4,5.jpg'),
-    },
-    {
-      src: getSrc('templates/5,6.jpg'),
-    },
-    {
-      src: getSrc('templates/7,8.jpg'),
-    },
-    {
-      src: getSrc('templates/9,10.jpg'),
-    },
-    {
-      src: getSrc('templates/11,12.jpg'),
-    },
-    {
-      title: TranslateService.localize('project.What-Is-Happiness.results-description'),
-      src: getSrc('pages/1.jpg'),
-    },
-    ...[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((index) => ({
+    // {
+    //   title: TranslateService.localize('project.What-Is-Happiness.prepare-description'),
+    //   src: getSrc('characters/1.jpg'),
+    // },
+    // {
+    //   src: getSrc('characters/2.jpg'),
+    // },
+    // {
+    //   src: getSrc('characters/3.jpg'),
+    // },
+    // {
+    //   title: TranslateService.localize('project.What-Is-Happiness.storyboard'),
+    //   src: getSrc('storyboard.jpg'),
+    // },
+    // {
+    //   title: TranslateService.localize('project.What-Is-Happiness.templates'),
+    //   src: getSrc('templates/1,2.jpg'),
+    // },
+    // {
+    //   src: getSrc('templates/3.jpg'),
+    // },
+    // {
+    //   src: getSrc('templates/4,5.jpg'),
+    // },
+    // {
+    //   src: getSrc('templates/5,6.jpg'),
+    // },
+    // {
+    //   src: getSrc('templates/7,8.jpg'),
+    // },
+    // {
+    //   src: getSrc('templates/9,10.jpg'),
+    // },
+    // {
+    //   src: getSrc('templates/11,12.jpg'),
+    // },
+    // {
+    //   title: TranslateService.localize('project.What-Is-Happiness.results-description'),
+    //   src: getSrc('pages/1.jpg'),
+    // },
+    ...[5, 6, 9, 11].map((index) => ({
       src: getSrc(`pages/${index}.jpg`),
     })),
-    {
-      title: TranslateService.localize('project.What-Is-Happiness.book-cover'),
-      src: getSrc('book-cover/1.jpg'),
-    },
-    {
-      src: getSrc('book-cover/2.jpg'),
-    },
-    {
-      src: getSrc('book-cover/3.jpg'),
-    },
-    {
-      src: getSrc('book-cover/4.jpg'),
-    },
-    {
-      src: getSrc('book-cover/final.jpg'),
-    },
+    // {
+    //   title: TranslateService.localize('project.What-Is-Happiness.book-cover'),
+    //   src: getSrc('book-cover/1.jpg'),
+    // },
+    // {
+    //   src: getSrc('book-cover/2.jpg'),
+    // },
+    // {
+    //   src: getSrc('book-cover/3.jpg'),
+    // },
+    // {
+    //   src: getSrc('book-cover/4.jpg'),
+    // },
+    // {
+    //   src: getSrc('book-cover/final.jpg'),
+    // },
   ],
 });
